3e088e3d80b08bc5afdc19b39f1c72b0bc164a20
Nicolas Bettenburg <nicbet@kuy.io>
2023-11-13 12:02:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# HTML Heex
+
+Enable HTML intellisense in the `*.heex` files.
+
+## Features
+
+This extension provides features for editing HTML code inside Heex files, including HTML completion, hover, definition jump, link editing range, and automatic insertion.
+It also supports HTML code inside `~H` code blocks.
+By using this extension, developers can work more efficiently when developing with Phoenix Framework.
---
+++ src/extension.ts
@@ -0,0 +1,278 @@
+import * as vscode from 'vscode'
+import {
+  commands,
+  CompletionList,
+  DocumentFilter,
+  Hover,
+  languages,
+  LinkedEditingRanges,
+  Range,
+  TextDocument,
+  Uri,
+  workspace,
+  Position,
+} from 'vscode'
+import {
+  getLanguageService,
+  LanguageService,
+  TokenType,
+  type TextDocument as LanguageServiceTextDocument,
+  type HTMLDocument,
+} from 'vscode-html-languageservice'
+import { activateAutoInsertion } from './autoInsertion'
+
+function isInsideElixirRegion(
+  languageService: LanguageService,
+  documentText: string,
+  offset: number
+) {
+  const scanner = languageService.createScanner(documentText)
+
+  let token = scanner.scan()
+  while (token !== TokenType.EOS) {
+    if (
+      token === TokenType.Unknown &&
+      offset >= scanner.getTokenOffset() &&
+      offset <= scanner.getTokenEnd()
+    ) {
+      const text = scanner.getTokenText()
+      return text.startsWith('%') && text.endsWith('%')
+    }
+
+    token = scanner.scan()
+  }
+
+  return false
+}
+
+type EmbeddedRegion = {
+  start: number
+  end: number
+}
+
+function getElixirVirtualContent(
+  languageService: LanguageService,
+  documentText: string
+): string {
+  const regions: EmbeddedRegion[] = []
+  const scanner = languageService.createScanner(documentText)
+
+  let token = scanner.scan()
+  while (token !== TokenType.EOS) {
+    if (token === TokenType.Unknown) {
+      const text = scanner.getTokenText()
+      if (text.startsWith('%') && text.endsWith('%')) {
+        const start = text.startsWith('%=') ? 2 : 1
+        regions.push({
+          start: scanner.getTokenOffset() + start,
+          end: scanner.getTokenEnd() - 1,
+        })
+      }
+    }
+
+    token = scanner.scan()
+  }
+
+  let content = documentText
+    .split('\n')
+    .map((line) => {
+      return ' '.repeat(line.length)
+    })
+    .join('\n')
+
+  regions.forEach((r) => {
+    content =
+      content.slice(0, r.start) +
+      documentText.slice(r.start, r.end) +
+      content.slice(r.end)
+  })
+
+  return content
+}
+
+const toLanguageServiceTextDocument = (
+  document: TextDocument
+): LanguageServiceTextDocument => ({
+  ...document,
+  uri: document.uri.toString(),
+})
+
+const toHTMLDocument = (
+  htmlLanguageService: LanguageService,
+  document: TextDocument
+): HTMLDocument => {
+  return htmlLanguageService.parseHTMLDocument(
+    toLanguageServiceTextDocument(document)
+  )
+}
+
+// SEE: https://code.visualstudio.com/api/language-extensions/embedded-languages
+export function activate(context: vscode.ExtensionContext) {
+  const htmlLanguageService = getLanguageService()
+  const virtualDocumentContents = new Map<string, string>()
+
+  const getVirtualDocumentUri = (
+    document: TextDocument,
+    position: Position
+  ): Uri => {
+    const documentText = document.getText()
+    const isElixir = isInsideElixirRegion(
+      htmlLanguageService,
+      documentText,
+      document.offsetAt(position)
+    )
+    const content = isElixir
+      ? getElixirVirtualContent(htmlLanguageService, documentText)
+      : documentText
+    const ext = isElixir ? 'ex' : 'html'
+
+    const originalUri = document.uri.toString(true)
+    virtualDocumentContents.set(originalUri, content)
+
+    const vdocUriString = `embedded-content://html/${encodeURIComponent(
+      originalUri
+    )}.${ext}`
+    return Uri.parse(vdocUriString)
+  }
+
+  const documentSelector: DocumentFilter = {
+    scheme: 'file',
+    language: 'heex',
+    pattern: '**/*.heex',
+  }
+
+  context.subscriptions.push(
+    workspace.registerTextDocumentContentProvider('embedded-content', {
+      provideTextDocumentContent: (uri) => {
+        const originalUri = uri.path
+          .slice(1)
+          .slice(0, uri.path.lastIndexOf('.') - 1)
+        const decodedUri = decodeURIComponent(originalUri)
+        return virtualDocumentContents.get(decodedUri)
+      },
+    })
+  )
+
+  context.subscriptions.push(
+    languages.registerHoverProvider(documentSelector, {
+      async provideHover(document, position) {
+        const vdocUri = getVirtualDocumentUri(document, position)
+
+        const results = await commands.executeCommand<Hover[]>(
+          'vscode.executeHoverProvider',
+          vdocUri,
+          position
+        )
+        return results[0]
+      },
+    })
+  )
+
+  context.subscriptions.push(
+    languages.registerCompletionItemProvider(
+      documentSelector,
+      {
+        async provideCompletionItems(document, position, _token, context) {
+          const vdocUri = getVirtualDocumentUri(document, position)
+
+          return await commands.executeCommand<CompletionList>(
+            'vscode.executeCompletionItemProvider',
+            vdocUri,
+            position,
+            context.triggerCharacter
+          )
+        },
+      },
+      '<'
+    )
+  )
+
+  context.subscriptions.push(
+    languages.registerDefinitionProvider(documentSelector, {
+      async provideDefinition(document, position) {
+        const vdocUri = getVirtualDocumentUri(document, position)
+
+        return await commands.executeCommand<vscode.Location[]>(
+          'vscode.executeDefinitionProvider',
+          vdocUri,
+          position
+        )
+      },
+    })
+  )
+
+  context.subscriptions.push(
+    languages.registerLinkedEditingRangeProvider(documentSelector, {
+      async provideLinkedEditingRanges(document, position, _token) {
+        const documentText = document.getText()
+        const isInsideElixir = isInsideElixirRegion(
+          htmlLanguageService,
+          documentText,
+          document.offsetAt(position)
+        )
+        if (isInsideElixir) {
+          return
+        }
+
+        const htmlDocument = toHTMLDocument(htmlLanguageService, document)
+        const ranges = htmlLanguageService.findLinkedEditingRanges(
+          toLanguageServiceTextDocument(document),
+          position,
+          htmlDocument
+        )
+        if (!ranges) {
+          return
+        }
+
+        return new LinkedEditingRanges(
+          ranges.map(
+            (r) =>
+              new Range(
+                r.start.line,
+                r.start.character,
+                r.end.line,
+                r.end.character
+              )
+          )
+        )
+      },
+    })
+  )
+
+  context.subscriptions.push(
+    activateAutoInsertion(async (kind, document, position) => {
+      const isInsideElixir = isInsideElixirRegion(
+        htmlLanguageService,
+        document.getText(),
+        document.offsetAt(position)
+      )
+      if (isInsideElixir) {
+        return ''
+      }
+
+      const htmlDocument = toHTMLDocument(htmlLanguageService, document)
+      const textDocument = toLanguageServiceTextDocument(document)
+
+      if (kind === 'autoClose') {
+        return (
+          htmlLanguageService.doTagComplete(
+            textDocument,
+            position,
+            htmlDocument
+          ) ?? ''
+        )
+      } else {
+        return (
+          htmlLanguageService.doQuoteComplete(
+            textDocument,
+            position,
+            htmlDocument
+          ) ?? ''
+        )
+      }
+    }, documentSelector)
+  )
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
